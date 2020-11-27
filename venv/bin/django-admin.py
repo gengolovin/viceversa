d48c74ea303296db94503ca355e0a3544a0ce676
gennady <netbooks@mail.ru>
2020-11-27 23:02:44 +0300

    The fist commit

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/users/gennady/Django-Projects/viceversa-project/viceversa/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
